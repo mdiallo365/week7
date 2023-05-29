3dddde9e4b2731a21c52e43a894b336a89550b9b
Mouctar Diallo <mouctardiallo2017@gmail.com>
2023-05-29 19:19:57 -0400

    first commit

---
+++ images/Scripts/main.js
@@ -0,0 +1,32 @@
+const navURLs = [
+    { name: "Home", url: "index.html" },
+    { name: "National Parks", url: "parks.html" },
+    { name: "Mountains", url: "mountains.html" },
+  ];
+  
+  function navLink(item) {
+    const a = document.createElement("a");
+    a.href = item.url;
+    a.innerHTML = item.name;
+    return a;
+  }
+  
+  document.addEventListener("DOMContentLoaded", () => {
+    const image = document.createElement("img");
+    image.src = "images/banner.jpg";
+    image.alt = "Banner Image";
+    const banner = document.createElement("div");
+    banner.classList.add("banner");
+    banner.appendChild(image);
+    document.body.prepend(banner);
+  
+    const nav = document.getElementById("nav");
+    navURLs.forEach((item) => nav.appendChild(navLink(item)));
+  
+    const logo = document.createElement("img");
+    logo.src = "images/logo.png";
+    logo.alt = "Logo";
+    logo.classList.add("logo");
+    nav.insertAdjacentElement("afterbegin", logo);
+  });
+  
